56a813c48a78cb59c2d7ae9f403fab3222cc271a
Saulo Pereira Simoes <ra2457003>
2026-06-02 08:55:59 -0300

    Funcional

--- src/java/com/template/MainController.java
+++ src/java/com/template/MainController.java
@@ -1,93 +1,143 @@
 package com.template;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-
-import java.sql.SQLException;
+import javafx.scene.control.cell.PropertyValueFactory;
+import java.util.List;
 
 public class MainController {
 
     @FXML
-    private Button btnCadastrarAction;
-
+    private TableView<ProfessorDTO> tblProfessor;
     @FXML
-    private Button btnEditar;
-
+    private TableColumn<ProfessorDTO, Integer> colId;
     @FXML
-    private Button btnExcluir;
+    private TableColumn<ProfessorDTO, String> colNome;
+    @FXML
+    private TableColumn<ProfessorDTO, String> colEmail;
+    @FXML
+    private TableColumn<ProfessorDTO, Double> colSalario;
+    @FXML
+    private TableColumn<ProfessorDTO, String> colDisciplina;
 
     @FXML
     private TextField intId;
-
     @FXML
     private TextField txtNome;
-
     @FXML
     private TextField txtEmail;
-
     @FXML
     private TextField txtDisciplina;
-
     @FXML
     private TextField numSalario;
 
     @FXML
-    private TableView<ProfessorDTO> tblVolei;
+    public void initialize() {
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        colSalario.setCellValueFactory(new PropertyValueFactory<>("salario"));
+        colDisciplina.setCellValueFactory(new PropertyValueFactory<>("disciplina"));
+
+        carregarProfessor();
+    }
+
+    private void carregarProfessor() {
+        try {
+            ProfessorDAO ProfDAO = new ProfessorDAO();
+            List<ProfessorDTO> listaProfessor = ProfDAO.listar();
+
+            ObservableList<ProfessorDTO> dadosTabela = FXCollections.observableArrayList(listaProfessor);
+            tblProfessor.setItems(dadosTabela);
+
+            System.out.println("Tabela atualizada");
+
+        } catch (Exception e) {
+            System.out.println("Erro" + e.getMessage());
+        }
+    }
 
     @FXML
-    private TableColumn<ProfessorDTO, Integer> colId;
+    private void carregarCampos() {
+        ProfessorDTO objProfessorDTO = tblProfessor.getSelectionModel().getSelectedItem();
 
-    @FXML
-    private TableColumn<ProfessorDTO, String> colNome;
-
-    @FXML
-    private void initialize() {
-
-        System.out.println("FXML loaded successfully!");
+        if (objProfessorDTO != null) {
+            intId.setText(String.valueOf(objProfessorDTO.getId()));
+            txtNome.setText(objProfessorDTO.getNome());
+            txtEmail.setText(objProfessorDTO.getEmail());
+            txtDisciplina.setText(objProfessorDTO.getDisciplina());
+            numSalario.setText(String.valueOf(objProfessorDTO.getSalario()));
+        }
     }
 
     @FXML
     private void btnCadastrarAction() {
-
         try {
-
             String nome = txtNome.getText();
             String email = txtEmail.getText();
             String disciplina = txtDisciplina.getText();
-
             double salario = Double.parseDouble(numSalario.getText());
+            ProfessorDTO ProfDTO = new ProfessorDTO(0, nome, email, salario, disciplina);
 
-            ProfessorDTO professor = new ProfessorDTO(
-                    0,
-                    nome,
-                    email,
-                    salario,
-                    disciplina
-            );
+            ProfessorDAO ProfDAO = new ProfessorDAO();
+            ProfDAO.inserir(ProfDTO);
 
-            ProfessorDAO dao = new ProfessorDAO();
-
-            dao.inserir(professor);
-
-            limparCampos();
-
-            System.out.println("Professor salvo");
+            System.out.println("Professor cadastrado");
+            carregarProfessor();
 
         } catch (Exception e) {
-
             System.out.println("Erro: " + e.getMessage());
         }
     }
 
-    private void limparCampos() {
+    @FXML
+    private void btnExcluirAction() {
+        try {
+            int id = Integer.parseInt(intId.getText());
 
+            ProfessorDAO ProfDAO = new ProfessorDAO();
+            ProfDAO.deletar(id);
+
+            System.out.println("Professor excluído");
+            carregarProfessor();
+            btnLimparAction();
+
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+
+    @FXML
+    private void btnLimparAction() {
         intId.clear();
         txtNome.clear();
         txtEmail.clear();
         txtDisciplina.clear();
         numSalario.clear();
+    }
+
+    @FXML
+    private void btnEditarAction() {
+        try {
+            int id = Integer.parseInt(intId.getText());
+            String nome = txtNome.getText();
+            String email = txtEmail.getText();
+            String disciplina = txtDisciplina.getText();
+            double salario = Double.parseDouble(numSalario.getText());
+
+            ProfessorDTO ProfDTO = new ProfessorDTO(id, nome, email, salario, disciplina);
+            ProfessorDAO ProfDAO = new ProfessorDAO();
+            ProfDAO.atualizar(ProfDTO);
+
+            System.out.println("Professor editado");
+            carregarProfessor();
+
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
     }
 }
